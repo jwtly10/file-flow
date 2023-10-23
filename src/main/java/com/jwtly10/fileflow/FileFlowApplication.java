6ba823785d722057179201fa03ac65e08997bb71
Josh Watley <watleyjosh@gmail.com>
2023-10-23 20:18:33 +0100

    Initial Project

---
+++ src/main/java/com/jwtly10/fileflow/FileFlowApplication.java
@@ -0,0 +1,13 @@
+package com.jwtly10.fileflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtly10/fileflow/FileFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtly10.fileflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
